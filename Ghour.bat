330fa33b1f3de0d179e1dc6c5b4927951f76a937
brunoalves8 <ba578731@gmail.com>
2025-03-26 02:52:25 +0000

    changes

--- Ghour.bat
+++ Ghour.bat
@@ -1,4 +1,4 @@
 @echo off
-cd "C:\Users\LENOVO\OneDrive\Ambiente de Trabalho\UMinho Mestrado\GestaoHoras\"
-python "C:\Users\LENOVO\OneDrive\Ambiente de Trabalho\UMinho Mestrado\GestaoHoras\gestaohoras.py"
+cd "C:\Users\LENOVO\OneDrive\Ambiente de Trabalho\UMinho Mestrado\Gestao-Horas\"
+python "C:\Users\LENOVO\OneDrive\Ambiente de Trabalho\UMinho Mestrado\Gestao-Horas\gestaohoras.py"
 pause
--- hideprompt.vbs
+++ hideprompt.vbs
@@ -1,3 +1,3 @@
 Set WshShell = CreateObject("WScript.Shell")
-WshShell.Run chr(34) & "C:\Users\LENOVO\OneDrive\Ambiente de Trabalho\UMinho Mestrado\GestaoHoras\Ghour.bat" & Chr(34), 0
+WshShell.Run chr(34) & "C:\Users\LENOVO\OneDrive\Ambiente de Trabalho\UMinho Mestrado\Gestao-Horas\Ghour.bat" & Chr(34), 0
 Set WshShell = Nothing
